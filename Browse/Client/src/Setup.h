a65470081cd6a398fec40ec80dc2f87d54062afb
Raphael Menges <unrealer2@yahoo.de>
2018-03-15 17:27:38 +0100

    Further setup.

--- Browse/Client/src/Setup.h
+++ Browse/Client/src/Setup.h
@@ -31,8 +31,8 @@ namespace setup
 #endif
 
 	// Window
-	static const bool	FULLSCREEN = false; // does not work in combination with eye tracker calibration
-	static const bool	MAXIMIZE_WINDOW = true | DEPLOYMENT | DEMO_MODE; // only implemented for Windows
+	static const bool	FULLSCREEN = true; // does not work in combination with eye tracker calibration
+	static const bool	MAXIMIZE_WINDOW = false; // only implemented for Windows
 	static const int	INITIAL_WINDOW_WIDTH = 1280;
 	static const int	INITIAL_WINDOW_HEIGHT = 720;
 
