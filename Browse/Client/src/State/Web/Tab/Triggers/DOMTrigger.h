a38a9f9ae963fa2b07c6115e25a0f012667c74bc
Raphael Menges <unrealer2@yahoo.de>
2018-05-24 15:12:55 +0200

    Trigger offsets changed.

--- Browse/Client/src/State/Web/Tab/Triggers/DOMTrigger.h
+++ Browse/Client/src/State/Web/Tab/Triggers/DOMTrigger.h
@@ -74,7 +74,7 @@ protected:
 private:
 
 	// Available offsets within DOM node in x direction
-	const std::vector<float> _offsets = { 0.3f, 0.7f, 0.5f, 0.4f, 0.6f, 0.2f, 0.8f, 0.1f, 0.9f };
+	const std::vector<float> _offsets = { 0.5f, 0.3f, 0.7f, 0.4f, 0.6f, 0.2f, 0.8f, 0.1f, 0.9f };
 
     // Update position of overlay
     void UpdatePositionOfOverlayFrame(bool isButton); // button or badge
